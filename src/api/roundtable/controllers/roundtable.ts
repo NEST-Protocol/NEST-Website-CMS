03c58a3a871e1888503819c29c7b471e286568f9
tunogya <tunogya@qq.com>
2022-11-02 12:54:27 +0800

    add table

---
+++ src/api/roundtable/controllers/roundtable.ts
@@ -0,0 +1,7 @@
+/**
+ * roundtable controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::roundtable.roundtable');
---
+++ src/api/roundtable/routes/roundtable.ts
@@ -0,0 +1,7 @@
+/**
+ * roundtable router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::roundtable.roundtable');
